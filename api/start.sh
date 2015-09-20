990708b656876c7358723a2eb60e894a58539e7e
=Raj Desikavinayagompillai <dnraj@hotmail.com>
2015-09-20 05:00:17 -0700

    Added files for techcrunch hack - stockfinder hack

---
+++ api/start.sh
@@ -0,0 +1,6 @@
+nohup python name.py 7080  &
+nohup python productURL.py 7081  &
+nohup python salePrice.py 7082  &
+nohup python sellerInfo.py 7083  &
+nohup python thumbnailImage.py 7084  &
+nohup python stock.py 7085  &
---
+++ api/thumbnailImage.py
@@ -0,0 +1,26 @@
+import web
+import sys
+import os, cgi
+import requests 
+import json
+from pprint import pprint
+urls = (
+    '/', 'index'
+)
+
+class index: 
+  def GET(self):
+	#for arg in sys.argv:
+	print "Content-Type: text/html\n"
+	query =	 os.environ.get('QUERY_STRING')
+	arguments = cgi.parse_qs(query) if query else {}
+	item_id='41299768'
+	json_data_input='thumbnailImage'
+
+	q_url='http://api.walmartlabs.com/v1/items/'+item_id+'?format=json&apiKey=<CHANGETOAPIKEY>' 
+	response=requests.get(q_url) 
+	json_data = json.loads(response.text)
+	return (json_data["thumbnailImage"])
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
